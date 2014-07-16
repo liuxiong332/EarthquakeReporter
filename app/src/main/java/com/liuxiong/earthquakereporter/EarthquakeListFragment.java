beb9ce25da2b22c4c58cc016dce7fb9095605259
liuxiong <liuxiong332@163.com>
2014-07-16 12:37:42 +0800

    create new project EarthquakeReporter

---
+++ app/src/main/java/com/liuxiong/earthquakereporter/EarthquakeListFragment.java
@@ -0,0 +1,14 @@
+package com.liuxiong.earthquakereporter;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+/**
+ * Created by liuxi_000 on 2014/7/16.
+ */
+public class EarthquakeListFragment extends ListFragment {
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/liuxiong/earthquakereporter/Quake.java
@@ -0,0 +1,7 @@
+package com.liuxiong.earthquakereporter;
+
+/**
+ * Created by liuxi_000 on 2014/7/16.
+ */
+public class Quake {
+}
